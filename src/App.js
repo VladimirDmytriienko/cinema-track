0155e314da5044e30e7d00f8074da199273db63c
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-06-14 21:37:05 +0300

    002 commit: add favorites

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
-import { useEffect, useState } from 'react';
-import axios from "axios";
 import './App.css';
-import MovieCard from './components/MovieCard';
-function App() {
-  const API_URL ="https://api.themoviedb.org/3"
-  const [movies, setMovies] = useState([])
+import Favorites from './Favorites';
+import Main from './Main';
 
-  const fetchMovies = async () => {
-    const {data: {results}} = await axios.get(`${API_URL}/discover/movie`, {
-      params: {
-        api_key: process.env.REACT_APP_MOVIE_API_KEY
-      }
-    })
-    setMovies(results)
-  }
 
-  useEffect(() => {
-    fetchMovies()
-  }, [])
-
-  const renderMovies = () => (
-    movies.map(movie => (
-      
-      <MovieCard
-        key={movie.id}
-        movie={movie}
-      />
-      
-    ))
-  )
-
+const App = () => {
+ 
   return (
     <div className="App">
       <header>
         <h1>Cinema track</h1>
       </header>
-      <div className="wrapper">
-        {renderMovies()}
-      </div>
+      
+      <Main/>
+      <Favorites/>
+      
     </div>
   );
-}
+};
 
 export default App;
