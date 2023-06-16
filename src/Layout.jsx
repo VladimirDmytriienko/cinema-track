e8f03e2d820cd7313bc99da7de8d723b7e080eb9
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-06-16 16:26:45 +0300

    003 commit add router and add custom hook

---
+++ src/Layout.jsx
@@ -0,0 +1,17 @@
+import { NavLink, Outlet } from "react-router-dom"
+
+export const Layout = () => {
+  return (
+    <>  
+        <header> 
+            <h1>Cinema track</h1>
+            <NavLink to="/">Main</NavLink>
+            <NavLink to="/movies">Movies</NavLink>
+            <NavLink to="/favorites">Favorites</NavLink>    
+        </header>
+        <main>
+            <Outlet></Outlet>
+        </main> 
+    </>
+  )
+}
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,60 +1,47 @@
 import { useEffect, useState } from 'react';
 import axios from "axios";
 import MovieCard from './components/MovieCard';
+import useFavorites from './custom-hook/useFavorites';
 
 const Main = () => {
-    const API_URL = "https://api.themoviedb.org/3";
-    const [movies, setMovies] = useState([]);
-    const [favorites, setFavorites] = useState([]);
-  
-    const fetchMovies = async () => {
-      const { data: { results } } = await axios.get(`${API_URL}/discover/movie`, {
+  const API_URL = "https://api.themoviedb.org/3";
+  const { favorites, toggleFavorite } = useFavorites();
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/discover/movie`, {
         params: {
-          api_key: process.env.REACT_APP_MOVIE_API_KEY
+          api_key: process.env.REACT_APP_MOVIE_API_KEY,
         }
       });
+      const { data: { results } } = response;
       setMovies(results);
-    };
-  
-    const fetchFavorites = () => {
-      const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-      setFavorites(favorites);
-    };
-  
-    useEffect(() => {
-      fetchMovies();
-      fetchFavorites();
-    }, []);
-  
-    const toggleFavorite = (movie) => {
-      const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-      const index = favorites.findIndex((fav) => fav.id === movie.id);
-  
-      if (index !== -1) {
-        favorites.splice(index, 1);
-      } else {
-        favorites.push(movie);
-      }
-  
-      localStorage.setItem("favorites", JSON.stringify(favorites));
-      fetchFavorites();
-    };
-  
-    const renderMovies = () => (
-      movies.map(movie => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          isFavorite={favorites.some(fav => fav.id === movie.id)}
-          toggleFavorite={toggleFavorite}
-        />
-      ))
-    );
+    } catch (error) {
+      alert.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const renderMovies = () => (
+    movies.map(movie => (
+      <MovieCard
+        key={movie.id}
+        movie={movie}
+        isFavorite={favorites.some(fav => fav.id === movie.id)}
+        toggleFavorite={toggleFavorite}
+      />
+    ))
+  );
+
   return (
     <div className="wrapper">
-        {renderMovies()}
+      {renderMovies()}
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
