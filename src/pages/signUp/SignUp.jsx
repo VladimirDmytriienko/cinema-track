de22485b071bdeb81e3faabd6bf844d26f6e0425
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-07-14 18:04:10 +0300

    deploing

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -5,7 +5,7 @@ import "./signUp.css"
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { user, signUp } = UserAuth();
+  const {  signUp } = UserAuth();
   const navigate = useNavigate()
 
   const handleSubmit = async (e) => {
