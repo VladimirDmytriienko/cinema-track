80e6cb052a6882fda0ea89d4bde6c40f74307c40
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2023-08-30 21:23:30 +0300

    fix: routes

--- src/App.js
+++ src/App.js
@@ -1,64 +1,10 @@
 import './App.css';
 import { AuthContextProvider } from './context/AuthContext';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Layout from './layout/Layout';
-import ErrorPage from './pages/ErrorPage';
-import Home from './home/Home';
-import Movies from './pages/movies-page/Movies';
-// import router from './routes/router'
+import { RouterProvider } from 'react-router-dom';
+
+import router from './routes/router'
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: '/cinema-track',
-      element: <Layout />,
-      // loader: maindLoader,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-          // loader: dashboardLoader,
-          // action: dashboardAction,
-  
-        },
-        {
-          path: 'movies',
-          element: <Movies />,
-        },
-        // {
-        //   path: 'tv',
-        //   element: <Tv />,
-        // },
-        // // {
-        // //   path: '/favorites',
-        // //   element: <Favorites />,
-        // // },
-        // {
-        //   path: 'search',
-        //   element: <SearchPage />,
-        // },
-        // {
-        //   path: 'login',
-        //   element: <Login />,
-        // },
-        // {
-        //   path: 'signup',
-        //   element: <SignUp />,
-        // },
-        // {
-        //   path: 'account',
-        //   element: <ProtectedRoute> <Account/> </ProtectedRoute> ,
-        // },
-        // {
-        //   path: ':movieId',
-        //   element: <Movie />,
-        //   errorElement: <ErrorPage />,
-        // }
-      ]
-    },
-  
-  ]);
 
   return (
     <div className="App">
